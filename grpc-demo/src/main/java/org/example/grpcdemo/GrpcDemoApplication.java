09d6e7948e3d96fea1f3ffeb6b93e09da95dd900
aloks <sarkarsworld@gmail.com>
2026-05-27 22:00:35 +0530

    first commit - proto file added to a grpc server project

---
+++ grpc-demo/src/main/java/org/example/grpcdemo/GrpcDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcDemoApplication.class, args);
+    }
+
+}
---
+++ grpc-demo/src/test/java/org/example/grpcdemo/GrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
