f5a60962b565989389c65a24e98e25c73eb14dc3
aloks <sarkarsworld@gmail.com>
2026-05-27 22:32:47 +0530

    stock trade server - added proto file and generated java classes

---
+++ grpc-stock-trade-server/src/main/java/org/example/grpcstocktradeserver/GrpcStockTradeServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpcstocktradeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcStockTradeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcStockTradeServerApplication.class, args);
+    }
+
+}
