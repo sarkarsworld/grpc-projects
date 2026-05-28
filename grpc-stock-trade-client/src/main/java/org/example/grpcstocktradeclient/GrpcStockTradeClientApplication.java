cd95ee8a38e736e6a5abae30bedebb9fc924fac6
aloks <sarkarsworld@gmail.com>
2026-05-28 11:44:47 +0530

    stock trade client - first commit

---
+++ grpc-stock-trade-client/src/main/java/org/example/grpcstocktradeclient/GrpcStockTradeClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpcstocktradeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcStockTradeClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcStockTradeClientApplication.class, args);
+    }
+
+}
---
+++ grpc-stock-trade-client/src/test/java/org/example/grpcstocktradeclient/GrpcStockTradeClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grpcstocktradeclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcStockTradeClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
