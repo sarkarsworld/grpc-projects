4b7451a0fe2a504335e699280cd8371d5dd190d7
aloks <sarkarsworld@gmail.com>
2026-05-29 12:42:32 +0530

    stock trade server - server side streaming code for gRPC stockTrade server.

--- grpc-stock-trade-server/src/main/java/org/example/grpcstocktradeserver/service/StockTradingServiceImpl.java
+++ grpc-stock-trade-server/src/main/java/org/example/grpcstocktradeserver/service/StockTradingServiceImpl.java
@@ -2,11 +2,15 @@ package org.example.grpcstocktradeserver.service;
 
 import com.example.generated.stockTrade.StockResponse;
 import com.example.generated.stockTrade.StockTradingServiceGrpc;
-import com.example.generated.stockTrade.StrockRequest;
+import com.example.generated.stockTrade.StockRequest;
 import io.grpc.stub.StreamObserver;
 import org.example.grpcstocktradeserver.entity.Stocks;
 import org.example.grpcstocktradeserver.repository.StocksRepo;
 import org.springframework.grpc.server.service.GrpcService;
+
+import java.time.Instant;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @GrpcService
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
@@ -17,8 +21,9 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
         this.stocksRepo = stocksRepo;
     }
 
+    // Example of Unary Request and Response using gRPC.
     @Override
-    public void getStockPrice(StrockRequest request, StreamObserver<StockResponse> responseObserver) {
+    public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
 
         // Get stock-name from StockRequest.
         String stockName = request.getStockName();
@@ -41,4 +46,35 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
 
 
     }
+
+    @Override
+    public void subscribeStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+
+        // Get stock-name from StockRequest.
+        String stockName = request.getStockName();
+
+        // Send hardcoded simulate stream of responses.
+        try {
+            for (int i=0; i<10; i++){
+                // Create object of StockResponse.
+                StockResponse stockResponse = StockResponse.newBuilder()
+                        .setStockName(stockName)
+                        .setPrice(new Random().nextDouble(200))
+                        .setTimestamp(Instant.now().toString())
+                        .build();
+
+                // send the response object via responseObserver and sleep for 1 second.
+                responseObserver.onNext(stockResponse);
+                TimeUnit.SECONDS.sleep(1);
+
+            }
+
+            // Mark the tasks as completed.
+            responseObserver.onCompleted();
+        }catch (Exception e){
+            responseObserver.onError(e);
+        }
+
+    }
+
 }
